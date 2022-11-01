66ff25e7a7b73bcfa7ffed2f04054c51e74e09fa
AkashQuo <akash.bora@quovantis.com>
2022-11-01 09:39:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Clock from './clockModule';
+import Page from './Page';
+import NameForm from './NameForm';
+import FlavorForm from './FlavorForm';
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+let x=["Akash","Akash2","Akash3"]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <div>
+        {x.length > 0 &&
+          <h2>
+            You have {x.length} people.
+          </h2>
+        }
+      </div> 
+      <div>
+        {x.map(function(emp_name){return (<Welcome name={emp_name}/>);})
+        }
+      </div>
+      <div>
+        {
+          <Clock />
+        }
+        {
+          <Page />
+        }
+        {
+          <NameForm />
+        }
+        {
+          <FlavorForm />
+        }
+      </div>
     </div>
   );
 }
 
+// const root = R~eactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
 export default App;
